d43f104ea74570814531dc3efcb4aea38643db36
nokko <13428215+nok-ko@users.noreply.github.com>
2022-05-07 01:29:55 -0700

    Fix silliness.

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import {readFile, opendir} from 'node:fs/promises';
 try {
   // Only validate files with .css extension. Hardcoded and bad.
   const ext = 'css';
-  const directoryName = core.getInput(directory);
+  const directoryName = core.getInput('directory');
   const directory = await opendir(directoryName);
   const errors = [];
 
